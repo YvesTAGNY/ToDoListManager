8e7f5dd8226763767efdcac95ca852c202b2ea14
Yves TAGNY <yves.tagny@yahoo.fr>
2017-01-12 22:18:43 +0100

    creation de la classe Service du serveur et modification de la classe serveur

--- Mini-ProjetPTandYT/src/application/Serveur.java
+++ Mini-ProjetPTandYT/src/application/Serveur.java
@@ -1,10 +1,21 @@
 package application;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Serveur {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static final int PORT = 5000;
+	
+	public static void main(String[] args) throws IOException {
+		
+		ServerSocket socketServeur = new ServerSocket(PORT);
+		do{
+			Socket socketClient = socketServeur.accept();
+			Thread t = new Thread(new Service(socketClient)) ;
+			t.start();
+		}while(true);
 	}
 
 }
