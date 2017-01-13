0548c5db06626ee81d6de2ba992a97fc07fd8d7d
e155453 <e155453@E-0604.univ-lemans.fr>
2017-01-13 15:54:53 +0100

    creation task et modif User

--- Mini-ProjetPTandYT/src/User.java
+++ Mini-ProjetPTandYT/src/User.java
@@ -3,11 +3,29 @@ import java.util.ArrayList;
 public class User {
 
 	private String UserName;
-	private ArrayList<String> ToDOList = new ArrayList<String>();
+	private ArrayList<Task> ToDOList = new ArrayList<Task>();
 	
-	User (String Name , ArrayList<String> t){
+	User (String Name , ArrayList<Task> t){
 		UserName=Name;
 		ToDOList=t;
 		
 	}
+	
+	String getUserName(){
+		return UserName;
+	}
+	
+	
+	 ArrayList<Task> getToDOList(){
+		return ToDOList;
+	}
+	 
+	 void setUserName(String Name){
+		 UserName=Name; 
+	 }
+	 
+ void setToDOList(ArrayList<Task> tdl){
+	 ToDOList=tdl;
+		 
+	 }
 }
